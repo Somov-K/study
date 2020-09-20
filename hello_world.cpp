ce793532b1f4fe4fec13321a2d292306b8361393
Somov-K <71546717+Somov-K@users.noreply.github.com>
2020-09-20 15:12:19 +0400

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,1 +1,12 @@
+using System;
 
+namespace Study_CVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
