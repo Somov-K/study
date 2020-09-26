c6746d3f077241342c906fd6f0ee2e307dce4da9
Kirill Somov <alexeysmv69@gmail.com>
2020-09-26 12:24:24 +0400

    Добавление функции проверки возраста

--- Study CVS/Study CVS/Program.cs
+++ Study CVS/Study CVS/Program.cs
@@ -9,6 +9,17 @@ namespace Study_CVS
             Console.Write("Введите свое имя: ");
             string x = Console.ReadLine();
             Console.WriteLine($"Привет, {x}!");
+            Console.WriteLine("Чтобы продолжить введите ваш IQ ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            if (y > 150) 
+            {
+                Console.WriteLine("Вы можете продолжить...");
+            }
+            else if (y < 150)
+            {
+                Console.WriteLine("Для продолжения необходимо иметь более выской показатель, тренеруйтесь!");
+            }
+
         }
     }
 }
